077473f8bb49c6a4289516bb28a18d053ce29ad7
Yaro WebDev <yg@think.studio>
2021-12-25 18:40:19 +0200

    Remove script wrapper from  display-scripts

--- resources/views/gpt/display-scripts.blade.php
+++ resources/views/gpt/display-scripts.blade.php
@@ -1,8 +1,6 @@
 <?php /** @var \AdDirector\GPT\Presenter $adPresenter */ ?>
-<!--AdDirector:GPT:display -->
-<script>
-    window.googletag = window.googletag || {cmd: []};
-    googletag.cmd.push(function () {
-        {!! $adPresenter->displayAdsScript() !!}
-    });
-</script>
+/* AdDirector:GPT:display */
+window.googletag = window.googletag || {cmd: []};
+googletag.cmd.push(function () {
+    {!! $adPresenter->displayAdsScript() !!}
+});
