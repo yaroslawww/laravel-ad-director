8214ac2020b755bd1e526a738f1cafe935a263dc
Yaro WebDev <yg@think.studio>
2022-01-02 15:57:22 +0200

    Update display script

--- resources/views/gpt/display-scripts.blade.php
+++ resources/views/gpt/display-scripts.blade.php
@@ -1,6 +1,11 @@
-<?php /** @var \AdDirector\GPT\Presenter $adPresenter */ ?>
+<?php /** @var \AdDirector\GPT\Presenter $adPresenter */
+// language=JavaScript
+?>
 /* AdDirector:GPT:display */
+function displayGPTAd() {
 window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function () {
-    {!! $adPresenter->displayAdsScript() !!}
-});
+    googletag.cmd.push(function () {
+        {!! $adPresenter->displayAdsScript() !!}
+    });
+}
+displayGPTAd();
