e0e7d30b2d8f0a9ddedffeb4bfe9ce96f2db516f
Yaro WebDev <yg@think.studio>
2023-08-06 14:01:20 +0300

    Update package

--- tests/Fixtures/TestServiceProvider.php
+++ tests/Fixtures/TestServiceProvider.php
@@ -13,18 +13,18 @@ class TestServiceProvider extends ServiceProvider
         AdDirector::gpt()
             // Add size with mapping example
                   ::addSize('leaderboard', new \AdDirector\GPT\Size(
-            // Allowed sizes
-                [[728, 90], [320, 50]],
-                // Responsive size map
-                (new SizeMap())
-                    // Tablet
-                    ->addSize([1024, 768], [728, 90])
-                    // Desktop
-                    ->addSize([1050, 200], [728, 90])
-                    // Mobile
-                    ->addSize([640, 480], [320, 50])
-                    // Any size
-                    ->addSize([0, 0], [320, 50])
+                      // Allowed sizes
+                      [[728, 90], [320, 50]],
+                      // Responsive size map
+                      (new SizeMap())
+                          // Tablet
+                          ->addSize([1024, 768], [728, 90])
+                          // Desktop
+                          ->addSize([1050, 200], [728, 90])
+                          // Mobile
+                          ->addSize([640, 480], [320, 50])
+                          // Any size
+                          ->addSize([0, 0], [320, 50])
                   ))
             // Add static size without responsive
                   ::addSize('medium_rectangle', new \AdDirector\GPT\Size([300, 250]));
